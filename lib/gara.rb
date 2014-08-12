acb7819f1f13c7fd8f0276ad86085fcd7b6c3de9
Steven Talcott Smith <steve@aelogica.com>
2014-08-12 17:14:32 +0800

    Update gemspec; only register template handler if Rails defined

--- lib/gara.rb
+++ lib/gara.rb
@@ -2,5 +2,7 @@ module Gara
   require 'gara/template/handler'
   require 'gara/delegator'
   require 'gara/html5_emitter'
-  ActionView::Template.register_template_handler :gara, Gara::Template::Handler
+  if defined?(Rails)
+    ::ActionView::Template.register_template_handler :gara, Gara::Template::Handler
+  end
 end
