453995acbcf3fa4ef6913ed372863f07f1be03db
Steven Talcott Smith <steve@aelogica.com>
2014-08-15 14:30:35 +0800

    Cleanup code ; tests pass

--- lib/gara/html5_emitter.rb
+++ lib/gara/html5_emitter.rb
@@ -34,50 +34,23 @@ module Gara
 
     def initialize
       @doc = Nokogiri::HTML::DocumentFragment.parse("")
-      @gara_delegate = Nokogiri::HTML::Builder.with(@doc)
+      @builder = Nokogiri::HTML::Builder.with(@doc)
     end
 
     def add_methods_to(context)
-
-      builder = @gara_delegate                   # create a local binding so we can access builder in an instance of Context
-      proc_hash = HTML5_TAGS.inject({}) do |hash, tag|
-          hash[tag] = -> (*args, &block) {
-            begin
-              builder.public_send(tag, *args) do # public send is necessary due to send accessing private method Kernel#p
-                unless block.nil?
-                  result = block.call            # necessary to make sure block executes in Context not Builder
-                  if result.kind_of?(String)
-                    self << result               # add any string returned to the document so that: p { "works" } yields "<p>works</p>"
-                  else
-                    result
-                  end
-                end
-              end
-            rescue Exception => e
-              binding.pry
-            end
-          }
-          hash
-        end
-
-
+      builder = @builder
       # Open the eigenclass of the passed in context so we can add the procs created above as tag methods
-      eigenclass_of_context = 
-        class << context ; self ; end
-      proc_hash.each do |method_name, proc|
+      eigenclass_of_context = class << context ; self ; end
+
+      _proc_hash(@builder).each do |method_name, proc|
         eigenclass_of_context.send(:define_method, method_name, &proc)
       end
+
       eigenclass_of_context.send(:define_method, :<<) do |string|
         builder << string if string.kind_of?(String)
       end
 
-      helper_methods = eigenclass_of_context.instance_methods
-      helper_methods -= HTML5_TAGS
-      helper_methods -= Object.instance_methods
-      helper_methods.reject! {|method| method.to_s.match(/(_\d+_\d+$)|(lookup_context)|(<<)/) }
-
-      helper_methods.each do |method|
-
+      _helper_methods_from(eigenclass_of_context).each do |method|
         eigenclass_of_context.class_eval do
           define_method method do |*args, &block_for_helper|
             result = super(*args, &block_for_helper)
@@ -101,6 +74,38 @@ module Gara
         @doc.to_html
       end
     end
+
+    private
+      def _helper_methods_from(eigenclass_of_context)
+        helpers = ((eigenclass_of_context.instance_methods - HTML5_TAGS) - Object.instance_methods)
+        helpers.reject do |method|
+          method.to_s.match(/(_\d+_\d+$)|(lookup_context)|(<<)/)
+        end
+      end
+
+      def _proc_hash(nokogiri)
+        proc_hash = HTML5_TAGS.inject({}) do |hash, tag|
+          # builder is now a local binding so we can access in an instance of Context from these procs
+          builder = nokogiri
+          hash[tag] = -> (*args, &block) {
+             begin
+               builder.public_send(tag, *args) do # public send is necessary due to send accessing private method Kernel#p
+                 unless block.nil?
+                   result = block.call            # necessary to make sure block executes in Context not Builder
+                   if result.kind_of?(String)
+                     self << result               # add any string returned to the document so that: p { "works" } yields "<p>works</p>"
+                   else
+                     result
+                   end
+                 end
+               end
+             rescue Exception => e
+               binding.pry
+             end
+           }
+           hash
+        end
+      end
 
   end
 end
