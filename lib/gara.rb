bd02fc69caaaf28d8db5d79945cb875f1a188a4d
Steven Talcott Smith <steve@aelogica.com>
2014-09-18 16:06:51 -0700

    Remove Context wrapper class - no longer needed

--- lib/gara.rb
+++ lib/gara.rb
@@ -4,15 +4,9 @@ module Gara
   require 'gara/renderer'
   require 'gara/expander'
   require 'gara/components'
-  require 'gara/context'
   extend Renderer
   if defined?(Rails)
     ::ActionView::Template.register_template_handler :gara, Gara::Template::Handler
-    ActionController::Base.class_eval do
-      def view_context
-        Gara::Context.new(view_renderer, view_assigns, self)
-      end
-    end
   end
 
 end
