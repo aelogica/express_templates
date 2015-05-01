101a650a7a1ecbff057a1df8d508fd5c938dc0f4
Steven Talcott Smith <steve@aelogica.com>
2015-04-30 23:20:25 -0700

    Give a useful message if a builder method is not implemented

--- lib/express_templates/components/capabilities/building.rb
+++ lib/express_templates/components/capabilities/building.rb
@@ -2,6 +2,9 @@ module ExpressTemplates
   module Components
     module Capabilities
       module Building
+        def method_missing(name, *args)
+          raise "#{self.class.to_s} has no method '#{name}'"
+        end
       end
     end
   end
