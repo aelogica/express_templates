5b46e1b92b7a08f535158102f05271619eb1fd22
Steven Talcott Smith <steve@aelogica.com>
2015-07-22 09:17:57 +0800

    [#92312640] new idea for components to work with designer

--- lib/express_templates/renderer.rb
+++ lib/express_templates/renderer.rb
@@ -1,8 +1,6 @@
 module ExpressTemplates
   module Renderer
-    # render accepts source or block, expands to macros
-    # compiles the resulting macros 
-    # and then evaluates the resulting string of ruby in the context provided
+    # render accepts source or block, evaluates the resulting string of ruby in the context provided
     def render context=nil, template_or_src=nil, &block
       compiled_template = compile(template_or_src, &block)
       context.instance_eval compiled_template
