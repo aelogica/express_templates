b4bfd451a267652cb03052142f5dc7a08892a45f
Steven Talcott Smith <steve@aelogica.com>
2015-08-25 13:49:28 +0800

    Add collection iterator "all" component

---
+++ lib/express_templates/components/all.rb
@@ -0,0 +1,26 @@
+module ExpressTemplates
+  module Components
+    class All < Container
+
+      has_argument :id, "Name of the collection", as: :collection_name, type: :symbol
+
+      contains -> (&block) {
+        prepended
+        collection.each do |item|
+          assigns[member_name] = item
+          block.call(self) if block
+        end
+        appended
+      }
+
+      def member_name
+        config[:collection_name].to_s.singularize.to_sym
+      end
+
+      def collection
+        self.send(config[:collection_name])
+      end
+
+    end
+  end
+end
